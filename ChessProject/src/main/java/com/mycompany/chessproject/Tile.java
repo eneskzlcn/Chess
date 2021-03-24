5ce240f3817d694609a9653edfe40526d86e23ad
Enes Kızılcan <nazifenes.kizilcin@stu.fsm.edu.tr>
2021-03-24 03:48:20 +0300

    tile behaviours is simply added

--- ChessProject/src/main/java/com/mycompany/chessproject/Tile.java
+++ ChessProject/src/main/java/com/mycompany/chessproject/Tile.java
@@ -10,5 +10,38 @@ package com.mycompany.chessproject;
  * @author Enes Kızılcın <nazifenes.kizilcin@stu.fsm.edu.tr>
  */
 public class Tile {
-    
+
+    private Piece piece;
+    private int x;
+    private int y;
+
+    public Tile(int x, int y, Piece piece) {
+        this.setPiece(piece);
+        this.setX(x);
+        this.setY(y);
+    }
+
+    public Piece getPiece() {
+        return this.piece;
+    }
+
+    public void setPiece(Piece p) {
+        this.piece = p;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }
