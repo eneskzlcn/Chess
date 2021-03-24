0aa3cc376b5f985c48f9e2672f6f86da9f4f600b
Enes Kızılcan <nazifenes.kizilcin@stu.fsm.edu.tr>
2021-03-24 03:48:50 +0300

    a piece object defined simply

--- ChessProject/src/main/java/com/mycompany/chessproject/Piece.java
+++ ChessProject/src/main/java/com/mycompany/chessproject/Piece.java
@@ -9,6 +9,31 @@ package com.mycompany.chessproject;
  *
  * @author Enes Kızılcın <nazifenes.kizilcin@stu.fsm.edu.tr>
  */
-public class Piece {
-    
+public abstract class Piece {
+
+    private boolean killed = false;
+    private boolean white = false;
+
+    public Piece(boolean white) {
+        this.setWhite(white);
+    }
+
+    public boolean isWhite() {
+        return this.white;
+    }
+
+    public void setWhite(boolean white) {
+        this.white = white;
+    }
+
+    public boolean isKilled() {
+        return this.killed;
+    }
+
+    public void setKilled(boolean killed) {
+        this.killed = killed;
+    }
+
+    public abstract boolean canMove(Board board,
+            Tile start, Tile end);
 }
