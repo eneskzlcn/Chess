8d2e7b70a10edb76b6399ea5792dcaa625bbc8d6
Enes Kızılcan <nazifenes.kizilcin@stu.fsm.edu.tr>
2021-03-26 02:06:11 +0300

    resources created

---
+++ ChessProject/src/main/java/chess_game_2/Resources.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chess_game_2;
+
+/**
+ *
+ * @author Enes Kızılcın <nazifenes.kizilcin@stu.fsm.edu.tr>
+ */
+public class Resources {
+    
+    public static int ROW_COUNT = 8;
+    public static int ROW_TILE_COUNT = 8;
+}
