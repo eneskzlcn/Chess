660e1309abf921ae659e97e9c87575fca3dc757d
Enes Kızılcan <nazifenes.kizilcin@stu.fsm.edu.tr>
2021-03-25 20:33:26 +0300

    reset back to old architecture

---
+++ ChessProject/src/main/java/com/mycompany/chessproject/PieceTypes.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.chessproject;
+
+/**
+ *
+ * @author Enes Kızılcın <nazifenes.kizilcin@stu.fsm.edu.tr>
+ */
+public enum PieceTypes
+{
+    BLACKQUEEN,
+    BLACKKING,
+    BLACKROOK,
+    BLACKBISHOP,
+    BLACKKNIGHT,
+    BLACKPAWN,
+    WHITEQUEEN,
+    WHITEKING,
+    WHITEROOK,
+    WHITEBISHOP,
+    WHITEKNIGHT,
+    WHITEPAWN,
+}
