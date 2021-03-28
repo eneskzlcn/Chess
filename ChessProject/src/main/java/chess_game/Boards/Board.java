497b2f9d7d31d7ae486bb755959be2a8518db595
Enes Kızılcan <nazifenes.kizilcin@stu.fsm.edu.tr>
2021-03-29 02:34:16 +0300

    Board initialization is halfly finisihed

--- ChessProject/src/main/java/chess_game/Boards/Board.java
+++ ChessProject/src/main/java/chess_game/Boards/Board.java
@@ -8,6 +8,8 @@ package chess_game.Boards;
 import chess_game.Pieces.*;
 import chess_game.Resources.*;
 import chess_game.Utilities.BoardUtilities;
+import javax.swing.JPanel;
+
 /**
  *
  * @author Enes Kızılcın <nazifenes.kizilcin@stu.fsm.edu.tr>
@@ -18,6 +20,30 @@ public class Board {
 
     public Tile getTile(Coordinate coordinate) {
         return getTile(coordinate.getX(), coordinate.getY());
+    }
+
+    public Board() {
+        //black
+        tiles = new Tile[BOARD_Configurations.ROW_COUNT][BOARD_Configurations.ROW_TILE_COUNT];
+        tiles[0][0] = new Tile(new Coordinate(0,0),new Rook(Team.BLACK));
+        tiles[1][0]= new Tile(new Coordinate(1,0),new Knight(Team.BLACK));
+        tiles[2][0]= new Tile(new Coordinate(2,0),new Bishop(Team.BLACK));
+        tiles[3][0]= new Tile(new Coordinate(3,0),new Queen(Team.BLACK));
+        tiles[4][0]= new Tile(new Coordinate(4,0),new King(Team.BLACK));
+        tiles[5][0]= new Tile(new Coordinate(5,0),new Bishop(Team.BLACK));
+        tiles[6][0]= new Tile(new Coordinate(6,0),new Knight(Team.BLACK));
+        tiles[7][0]= new Tile(new Coordinate(7,0),new Rook(Team.BLACK));
+
+        //white
+        tiles[0][7] = new Tile(new Coordinate(0,7),new Rook(Team.BLACK));
+        tiles[1][7]= new Tile(new Coordinate(1,7),new Knight(Team.BLACK));
+        tiles[2][7]= new Tile(new Coordinate(2,7),new Bishop(Team.BLACK));
+        tiles[3][7]= new Tile(new Coordinate(3,7),new King(Team.BLACK));
+        tiles[4][7]= new Tile(new Coordinate(4,7),new Queen(Team.BLACK));
+        tiles[5][7]= new Tile(new Coordinate(5,7),new Bishop(Team.BLACK));
+        tiles[6][7]= new Tile(new Coordinate(6,7),new Knight(Team.BLACK));
+        tiles[7][7]= new Tile(new Coordinate(7,7),new Rook(Team.BLACK));
+
     }
 
     public Tile getTile(int x, int y) {
@@ -41,6 +67,7 @@ public class Board {
         }
         return null;
     }
+
     public Tile getTileOfGivenTeamPiece(Team team, PieceTypes pieceType) {
         for (int i = 0; i < BOARD_Configurations.ROW_COUNT; i++) {
             for (int j = 0; j < BOARD_Configurations.ROW_TILE_COUNT; j++) {
