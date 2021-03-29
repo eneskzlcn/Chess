6b22f5415c82ce0b42b077c1538541664f8f39af
Enes Kızılcan <nazifenes.kizilcin@stu.fsm.edu.tr>
2021-03-29 19:28:21 +0300

    useless function in Board removed

--- ChessProject/src/main/java/chess_game/Boards/Board.java
+++ ChessProject/src/main/java/chess_game/Boards/Board.java
@@ -16,7 +16,7 @@ import javax.swing.JPanel;
  */
 public class Board {
 
-    Tile[][] tiles;
+    private final Tile[][] tiles = new Tile[BOARD_Configurations.ROW_COUNT][BOARD_Configurations.ROW_TILE_COUNT];
 
     public Tile getTile(Coordinate coordinate) {
         return getTile(coordinate.getX(), coordinate.getY());
@@ -24,7 +24,6 @@ public class Board {
 
     public Board() {
         //black
-        tiles = new Tile[BOARD_Configurations.ROW_COUNT][BOARD_Configurations.ROW_TILE_COUNT];
         tiles[0][0] = new Tile(new Coordinate(0,0),new Rook(Team.BLACK));
         tiles[1][0]= new Tile(new Coordinate(1,0),new Knight(Team.BLACK));
         tiles[2][0]= new Tile(new Coordinate(2,0),new Bishop(Team.BLACK));
@@ -33,16 +32,28 @@ public class Board {
         tiles[5][0]= new Tile(new Coordinate(5,0),new Bishop(Team.BLACK));
         tiles[6][0]= new Tile(new Coordinate(6,0),new Knight(Team.BLACK));
         tiles[7][0]= new Tile(new Coordinate(7,0),new Rook(Team.BLACK));
-
+        
+        for(int i= 0; i< 8; i++)
+        {
+            tiles[i][1] = new Tile(new Coordinate(i,1),new Pawn(Team.BLACK));
+            tiles[i][6] = new Tile(new Coordinate(i,6),new Pawn(Team.WHITE));
+        }
+        for(int i = 2; i <6; i++)
+        {
+            for(int j = 0; j <8;j++)
+            {
+                tiles[j][i] = new Tile(new Coordinate(j,i),null);
+            }
+        }
         //white
-        tiles[0][7] = new Tile(new Coordinate(0,7),new Rook(Team.BLACK));
-        tiles[1][7]= new Tile(new Coordinate(1,7),new Knight(Team.BLACK));
-        tiles[2][7]= new Tile(new Coordinate(2,7),new Bishop(Team.BLACK));
-        tiles[3][7]= new Tile(new Coordinate(3,7),new King(Team.BLACK));
-        tiles[4][7]= new Tile(new Coordinate(4,7),new Queen(Team.BLACK));
-        tiles[5][7]= new Tile(new Coordinate(5,7),new Bishop(Team.BLACK));
-        tiles[6][7]= new Tile(new Coordinate(6,7),new Knight(Team.BLACK));
-        tiles[7][7]= new Tile(new Coordinate(7,7),new Rook(Team.BLACK));
+        tiles[0][7] = new Tile(new Coordinate(0,7),new Rook(Team.WHITE));
+        tiles[1][7]= new Tile(new Coordinate(1,7),new Knight(Team.WHITE));
+        tiles[2][7]= new Tile(new Coordinate(2,7),new Bishop(Team.WHITE));
+        tiles[3][7]= new Tile(new Coordinate(3,7),new King(Team.WHITE));
+        tiles[4][7]= new Tile(new Coordinate(4,7),new Queen(Team.WHITE));
+        tiles[5][7]= new Tile(new Coordinate(5,7),new Bishop(Team.WHITE));
+        tiles[6][7]= new Tile(new Coordinate(6,7),new Knight(Team.WHITE));
+        tiles[7][7]= new Tile(new Coordinate(7,7),new Rook(Team.WHITE));
 
     }
 
