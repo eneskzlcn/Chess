fcb76a1640c1d6fc191602620031a7c61fb6fa54
Enes Kızılcan <nazifenes.kizilcin@stu.fsm.edu.tr>
2021-04-02 21:04:47 +0300

    BoardPanel updateGUI function created and filled

--- ChessProject/src/main/java/gui/BoardPanel.java
+++ ChessProject/src/main/java/gui/BoardPanel.java
@@ -29,19 +29,30 @@ public class BoardPanel extends JPanel {
         this.boardTiles = new TilePanel[BOARD_Configurations.ROW_COUNT][BOARD_Configurations.ROW_TILE_COUNT];
         for (int i = 0; i < BOARD_Configurations.ROW_COUNT; i++) {
             for (int j = 0; j < BOARD_Configurations.ROW_TILE_COUNT; j++) {
-                TilePanel tilePanel = new TilePanel(this, new Coordinate(j, i),chessBoard);
+                TilePanel tilePanel = new TilePanel(this, new Coordinate(j, i), chessBoard);
                 this.boardTiles[i][j] = tilePanel;
                 add(tilePanel);
             }
 
         }
     }
- 
+
     public TilePanel[][] getBoardTiles() {
         return boardTiles;
     }
 
     public void setBoardTiles(TilePanel[][] boardTiles) {
         this.boardTiles = boardTiles;
+    }
+
+    public void updateBoardGUI(Board board) {
+        for (int i = 0; i < BOARD_Configurations.ROW_COUNT; i++) {
+            for (int j = 0; j < BOARD_Configurations.ROW_TILE_COUNT; j++) {
+                boardTiles[i][j].assignTileColor(board);
+                boardTiles[i][j].assignTilePieceIcon(board);
+                
+            }
+
+        }
     }
 }
