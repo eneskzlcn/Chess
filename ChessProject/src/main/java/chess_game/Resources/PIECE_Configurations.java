5eac7f5abbc180c5a98941b686f4c5ae089cd9cc
Enes Kızılcan <nazifenes.kizilcin@stu.fsm.edu.tr>
2021-04-04 05:48:04 +0300

    PAWN_MOVES and its functionalities added to piece configurations

--- ChessProject/src/main/java/chess_game/Resources/PIECE_Configurations.java
+++ ChessProject/src/main/java/chess_game/Resources/PIECE_Configurations.java
@@ -6,7 +6,10 @@
 package chess_game.Resources;
 
 import chess_game.Pieces.Coordinate;
+import chess_game.Pieces.Team;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -16,8 +19,57 @@ public class PIECE_Configurations {
     
     //this coordinates means a knıght can make 8 different moves in a possible position. Its current position plus this positions shows its possible move coordinates.
     public static Coordinate[] KNIGHT_MOVES = {new Coordinate(2,1),new Coordinate(-2,1),new Coordinate(2,-1),new Coordinate(-2,-1),new Coordinate(1,2),new Coordinate(-1,2),new Coordinate(1,-2),new Coordinate(-1,-2)};
+    
     //this coordinates shows a bishops available move directions from its current position;
     public static Coordinate[] BISHOP_MOVES = {new Coordinate(1,1),new Coordinate(-1,1),new Coordinate(1,-1),new Coordinate(-1,-1)};
-
+    
+    //this coordinates shows a black pawns available movement directions from its current position
+    public static Coordinate[] BLACK_PAWN_NORMAL_MOVES = {new Coordinate(0,1)};
+    
+    //this coordinates shows a white pawns available movement directions from its current position
+    public static Coordinate[] WHITE_PAWN_NORMAL_MOVES = {new Coordinate(0,-1)};
+    
+    public static Coordinate[] WHITE_PAWN_ATTACK_MOVES = {new Coordinate(1,-1),new Coordinate(-1,-1)};
+    
+    public static Coordinate[] BLACK_PAWN_ATTACK_MOVES = {new Coordinate(1,1),new Coordinate(-1,1)};
+    
+    public static Coordinate[] BLACK_PAWN_START_MOVES = {new Coordinate(0,2)};
+    
+    public static Coordinate[] WHITE_PAWN_START_MOVES = {new Coordinate(0,-2)};
+    
+    public static int BLACK_PAWNS_START_Y_POS = 1;
+    
+    public static int WHITE_PAWNS_START_Y_POS = 6;
+    
+    public static Map <Team, Map> PAWN_MOVES;
+    
+    static
+    {
+        PAWN_MOVES = new HashMap<>();
+        Map <String, Coordinate[]> whitePawnMoves = new HashMap<>();
+        Map <String, Coordinate[]> blackPawnMoves = new HashMap<>();
+        whitePawnMoves.put("Normal", WHITE_PAWN_NORMAL_MOVES);
+        whitePawnMoves.put("Attack", WHITE_PAWN_ATTACK_MOVES);
+        whitePawnMoves.put("Start", WHITE_PAWN_START_MOVES);
+        blackPawnMoves.put("Normal", BLACK_PAWN_NORMAL_MOVES);
+        blackPawnMoves.put("Attack", BLACK_PAWN_ATTACK_MOVES);
+        blackPawnMoves.put("Start", BLACK_PAWN_START_MOVES);
+        PAWN_MOVES.put(Team.WHITE, whitePawnMoves);
+        PAWN_MOVES.put(Team.BLACK, blackPawnMoves);
+ 
+    }
+    
+    
+    public static int getPawnStartPosY(Team team)
+    {
+        if(team == Team.WHITE)
+        {
+            return WHITE_PAWNS_START_Y_POS; 
+        }
+        else
+        {
+            return BLACK_PAWNS_START_Y_POS;
+        }
+    }
 }
 
