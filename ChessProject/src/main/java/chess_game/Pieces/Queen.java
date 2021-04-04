51ff16804fa04f69885f8b4d0ac15c5b9feb3cf4
Enes Kızılcan <nazifenes.kizilcin@stu.fsm.edu.tr>
2021-04-04 17:55:57 +0300

    Queen movements completed

--- ChessProject/src/main/java/chess_game/Pieces/Queen.java
+++ ChessProject/src/main/java/chess_game/Pieces/Queen.java
@@ -8,6 +8,9 @@ package chess_game.Pieces;
 import chess_game.Boards.Board;
 import chess_game.Pieces.Move;
 import chess_game.Boards.Tile;
+import chess_game.Resources.PIECE_Configurations;
+import chess_game.Utilities.BoardUtilities;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -16,14 +19,36 @@ import java.util.List;
  */
 public class Queen extends Piece {
 
-    public Queen(Team team)
-    {
-        super(team,PieceTypes.QUEEN);
+    public Queen(Team team) {
+        super(team, PieceTypes.QUEEN);
     }
-  
+
     @Override
-    public List<Move> availableMoves(Board board, Coordinate currentCoordinate) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public List<Move> availableMoves(Board board, Coordinate currentCoord) {
+
+        List<Move> possibleMoves = new ArrayList<Move>();
+        Tile currentTile = board.getTile(currentCoord);
+        Tile destinationTile;
+        Coordinate destinationCoordinate;
+        for (Coordinate coord : PIECE_Configurations.QUUEN_MOVES) {
+            destinationCoordinate = currentCoord;
+            while (BoardUtilities.isValidCoordinate(destinationCoordinate.plus(coord))) {
+                destinationCoordinate = destinationCoordinate.plus(coord);
+                destinationTile = board.getTile(destinationCoordinate);
+                if (!destinationTile.hasPiece()) {
+                    possibleMoves.add(new Move(board, currentTile, destinationTile));
+                } else {
+                    if (destinationTile.getPiece().getTeam() != this.getTeam()) {
+                        possibleMoves.add(new Move(board, currentTile, destinationTile));
+                        break;
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+        return possibleMoves;
+
     }
-    
+
 }
