84d924a9bbed08b49042ad6db17269f5995df6dc
Enes Kızılcan <nazifenes.kizilcin@stu.fsm.edu.tr>
2021-04-05 23:39:47 +0300

    check state control func. added move utilities

--- ChessProject/src/main/java/chess_game/Utilities/MoveUtilities.java
+++ ChessProject/src/main/java/chess_game/Utilities/MoveUtilities.java
@@ -7,7 +7,11 @@ package chess_game.Utilities;
 
 import chess_game.Boards.Board;
 import chess_game.Boards.Tile;
+import chess_game.Pieces.Coordinate;
 import chess_game.Pieces.Move;
+import chess_game.Pieces.PieceTypes;
+import chess_game.Pieces.Team;
+import chess_game.Resources.PIECE_Configurations;
 
 /**
  *
@@ -22,6 +26,54 @@ public class MoveUtilities {
         for (Move m : board.getChosenTile().getPiece().availableMoves(board, board.getChosenTile().getCoordinate())) {
             if (m.getDestinationTile().getCoordinate().equals(destinationTile.getCoordinate())) {
                 return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean controlCheckState(Board board, Team team, Coordinate currentCoord) {
+
+        //control is there a knight as a danger for king ( check stete),
+        //control is there a bishop, queen, etc. ( check danger from cross way)
+        //control is there a rooks, queen etc. (check danger from straight way)
+        
+        Tile destinationTile;
+
+        for (Coordinate coord : PIECE_Configurations.KNIGHT_MOVES) {
+
+            if (!BoardUtilities.isValidCoordinate(currentCoord.plus(coord))) {
+                continue; // chech if the coord outside of board.
+            }
+            destinationTile = board.getTile(currentCoord.plus(coord));
+
+            if (!destinationTile.hasPiece()) {
+                continue;
+            } else {
+                if (destinationTile.getPiece().getTeam() != team && destinationTile.getPiece().getType() == PieceTypes.KNIGHT) {
+                    return true;
+                }
+            }
+        }
+        Tile currentTile = board.getTile(currentCoord);
+        Coordinate destinationCoordinate;
+        for (Coordinate coord : PIECE_Configurations.ROOK_MOVES) {
+            destinationCoordinate = currentCoord;
+            while (BoardUtilities.isValidCoordinate(destinationCoordinate.plus(coord))) {
+                destinationCoordinate = destinationCoordinate.plus(coord);
+                destinationTile = board.getTile(destinationCoordinate);
+                if (!destinationTile.hasPiece()) {
+                    continue;
+                } else {
+                    if(destinationTile.getPiece().getTeam()== team)
+                    {
+                        break;
+                    }
+                    if (destinationTile.getPiece().getTeam() != team && (destinationTile.getPiece().getType() == PieceTypes.ROOK ||destinationTile.getPiece().getType() == PieceTypes.QUEEN )) {
+                        return true;
+                    } else {
+                        break;
+                    }
+                }
             }
         }
         return false;
