841537151c28696f9cbd0668d6b377ddafe9ff27
Enes Kızılcan <nazifenes.kizilcin@stu.fsm.edu.tr>
2021-04-05 23:39:26 +0300

    comment added

--- ChessProject/src/main/java/chess_game/Pieces/PieceTypes.java
+++ ChessProject/src/main/java/chess_game/Pieces/PieceTypes.java
@@ -9,6 +9,8 @@ package chess_game.Pieces;
  *
  * @author Enes Kızılcın <nazifenes.kizilcin@stu.fsm.edu.tr>
  */
+
+//This enum is using for surely define the type of pieces.
 public enum PieceTypes
 {
     QUEEN,
