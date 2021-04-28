c7d420dd1953fdd60fd9ef732a7c9662af3be9d6
Enes Kızılcan <nazifenes.kizilcin@stu.fsm.edu.tr>
2021-04-28 15:25:29 +0300

    main class (test) for activate server is created

---
+++ ChessProject/src/main/java/server/Test.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Enes Kızılcın <nazifenes.kizilcin@stu.fsm.edu.tr>
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        Server server = new Server(4000);
+        server.Listen();
+
+        while (!server.socket.isClosed()) {
+
+            try {
+                System.out.println("Ben burada bekliyorum");
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+}
